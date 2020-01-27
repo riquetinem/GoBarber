35f12ab04be8b7ca05ae9f271c32a335ae89c0c0
Henrique Shinhate Tinem <henrique714tinem@gmail.com>
2020-01-27 18:22:12 -0300

    Validations

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,9 +3,16 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
+import authMiddleware from './app/middlewares/AuthMiddleware';
+
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
+// só se aplica as rotas que estão de baixo do middleware
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
 
 export default routes;
