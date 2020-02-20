947523cfa7d66c0256fcb50b1eed33f301f39a5c
Henrique Shinhate Tinem <henrique714tinem@gmail.com>
2020-02-20 02:38:36 -0300

    Customize profile page and header with user

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,7 +1,51 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { signOut } from '~/store/modules/auth/actions';
+import { updateProfileRequest } from '~/store/modules/user/actions';
+
+import AvatarInput from './AvatarInput';
+
+import { Container } from './styles';
 
 export default function Profile() {
-  return <div />;
+  const profile = useSelector(state => state.user.profile);
+
+  const dispatch = useDispatch();
+
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
+
+  function handleSignOut() {
+    dispatch(signOut());
+  }
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <AvatarInput name="avatar_id" />
+
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="example@gmail.com" />
+
+        <hr />
+
+        <Input type="password" name="oldPassword" placeholder="Senha Atual" />
+        <Input type="password" name="password" placeholder="Nova senha" />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirmação da senha"
+        />
+
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+
+      <button type="button" onClick={handleSignOut}>
+        Sair do GoBarber
+      </button>
+    </Container>
+  );
 }
--- frontend/src/store/modules/user/sagas.js
+++ frontend/src/store/modules/user/sagas.js
@@ -1,3 +1,30 @@
-import { all } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
-export default all([]);
+import api from '~/services/api';
+
+import { updateProfileFailure, updateProfileSuccess } from './actions';
+
+export function* updateProfile({ payload }) {
+  try {
+    const { name, email, avatar_id, ...rest } = payload.data;
+
+    const profile = {
+      name,
+      email,
+      avatar_id,
+      ...(rest.oldPassword ? rest : {}),
+    };
+
+    const res = yield call(api.put, '/users', profile);
+
+    toast.success('Perfil atualizado com sucesso');
+
+    yield put(updateProfileSuccess(res.data));
+  } catch (err) {
+    toast.error('Erro ao atualizar perfil, tente novamente mais tarde');
+    yield put(updateProfileFailure());
+  }
+}
+
+export default all([takeLatest('@user/UPDATE_PROFILE_REQUEST', updateProfile)]);
