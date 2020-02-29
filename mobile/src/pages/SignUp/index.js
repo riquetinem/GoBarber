2b8b842e8259dece22b9e136a195fffa9f18b3d6
Henrique Shinhate Tinem <henrique714tinem@gmail.com>
2020-02-29 16:56:05 -0300

    Signup page

--- mobile/src/pages/SignUp/index.js
+++ mobile/src/pages/SignUp/index.js
@@ -1,12 +1,54 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Image } from 'react-native';
+
+import logo from '../../assets/logo.png';
 
 import Background from '../../components/Background';
 
-export default function SignUp() {
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
+
+export default function SignUp({ navigation }) {
   return (
     <Background>
-      <Text>SignUp</Text>
+      <Container>
+        <Image source={logo} />
+
+        <Form>
+          <FormInput
+            icon="person-outline"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Nome completo"
+          />
+
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+          />
+
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Digite sua senha"
+          />
+
+          <SubmitButton onPress={() => {}}>Criar conta</SubmitButton>
+        </Form>
+
+        <SignLink onPress={() => navigation.navigate('SignIn')}>
+          <SignLinkText>Ja possuo uma conta</SignLinkText>
+        </SignLink>
+      </Container>
     </Background>
   );
 }
