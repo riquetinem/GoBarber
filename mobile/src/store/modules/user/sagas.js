f239270678951e031e2160d7adcd3b792576831a
Henrique Shinhate Tinem <henrique714tinem@gmail.com>
2020-03-01 19:18:01 -0300

    Page de profile

--- mobile/src/store/modules/user/sagas.js
+++ mobile/src/store/modules/user/sagas.js
@@ -7,14 +7,15 @@ import { updateProfileFailure, updateProfileSuccess } from './actions';
 
 export function* updateProfile({ payload }) {
   try {
-    const { name, email, avatar_id, ...rest } = payload.data;
+    const { name, email, ...rest } = payload.data;
 
     const profile = {
       name,
       email,
-      avatar_id,
       ...(rest.oldPassword ? rest : {}),
     };
+
+    console.tron.log(profile);
 
     const res = yield call(api.put, '/users', profile);
 
