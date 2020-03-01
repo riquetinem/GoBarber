43c6e92c83f3b1ef4770507482e1dec0b103fb38
Henrique Shinhate Tinem <henrique714tinem@gmail.com>
2020-03-01 18:09:26 -0300

    Page de appointments

--- mobile/src/pages/Dashboard/index.js
+++ mobile/src/pages/Dashboard/index.js
@@ -1,10 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
+import api from '~/services/api';
+
 import Background from '~/components/Background';
+import Appointment from '~/components/Appointment';
+
+import { Container, Title, List } from './styles';
 
 export default function Dashboard() {
-  return <Background />;
+  const [appointments, setAppointments] = useState([]);
+
+  useEffect(() => {
+    async function loadAppointments() {
+      const res = await api.get('/appointments');
+
+      setAppointments(res.data);
+    }
+
+    loadAppointments();
+  }, []);
+
+  async function handleCancel(id) {
+    const res = await api.delete(`/appointments/${id}`);
+
+    setAppointments(
+      appointments.map(appointment =>
+        appointment.id === id
+          ? {
+              ...appointment,
+              canceled_at: res.data.canceled_at,
+            }
+          : appointment
+      )
+    );
+  }
+
+  return (
+    <Background>
+      <Container>
+        <Title>Agendamentos</Title>
+
+        <List
+          data={appointments}
+          keyExtractor={item => String(item.id)}
+          renderItem={({ item }) => (
+            <Appointment data={item} onCancel={() => handleCancel(item.id)} />
+          )}
+        />
+      </Container>
+    </Background>
+  );
 }
 
 Dashboard.navigationOptions = {
